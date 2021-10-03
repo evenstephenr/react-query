3c16c4dcc81d976f415b589465236e781ab16c56
evenstephenr <github@ramirs.com>
2021-10-02 22:54:18 -0400

    pre-compile ts file

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   target: 'web',
-  entry: './src/index.ts',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'index.js',
